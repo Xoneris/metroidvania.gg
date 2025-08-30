e4692d08dc36a7e15129ebe96f5573f37803290e
Xoneris <kely42@gmx.ch>
2025-08-30 17:11:22 +0200

    Made the ad text boxes dynamic and added different texts that random get selected.

--- resources/js/Components/AdComponent.tsx
+++ resources/js/Components/AdComponent.tsx
@@ -3,6 +3,16 @@ import { useEffect } from "react";
 export default function AdComponents(props:any) {
     const { dataAdSlot, adWidth, adHeight } = props;
 
+    const adtexts = [
+        "Support MV.GG by whitelisting this site in your Adblock. No intrusive ads I promise!",
+        "Running this site costs about ~20$ each month. Consider disabling your Adblock!",
+        "Silksong is releasing soon, can you believe it? It's actually happening! Holy.",
+        "Did you know: About ~33% of people using the Internet use Adblock.",
+        "This is where I could promote your cool Metroidvania. Interested? metroidvaniagg@gmail.com.",
+        "Funfact: Symphony of the Night initally underperformed in sales. It's popularity came later.",
+        "Funfact: The term Metroidvania was invented by Fans and not by developers!"
+    ]
+
     useEffect(() => {
 
         try {
@@ -19,11 +29,14 @@ export default function AdComponents(props:any) {
     return (
         <div className="relative w-full p-4 flex justify-center items-center">
 
-            {/* <div className="absolute m-8 w-[calc(100%-64px)] h-[calc(100%-64px)] p-4 top-0 left-0 flex justify-center items-center rounded-lg border border-black bg-[#ccc]">
+            <div 
+                className="absolute p-4 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center rounded-lg border border-black bg-[#ccc]" 
+                style={{width:adWidth, height:adHeight}}
+            >
                 <p className="text-sm text-[#333] text-center">
-                    Support MV.GG by whitelisting this site in your Adblock. No intrusive ads I promise!
+                    {adtexts[Math.floor(Math.random() * adtexts.length)]}
                 </p>
-            </div> */}
+            </div>
 
             <ins className="adsbygoogle"
                 style={{display:"inline-block", width:adWidth, height:adHeight}}
