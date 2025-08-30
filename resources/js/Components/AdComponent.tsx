94c0ae90fd2a02316a33c032457055ab3c957a87
Xoneris <kely42@gmx.ch>
2025-08-30 05:04:42 +0200

    Created a fallback box for the ads if they are unavailable or blocked.

--- resources/js/Components/AdComponent.tsx
+++ resources/js/Components/AdComponent.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 
-
 export default function AdComponents(props:any) {
     const { dataAdSlot, adWidth, adHeight } = props;
 
@@ -18,8 +17,13 @@ export default function AdComponents(props:any) {
     },[]);
 
     return (
-        <div className="w-full p-4 flex justify-center items-center">
-        {/* <div className="bg-[#bbb] m-4 w-[calc(100%-32px)] h-20"> */}
+        <div className="relative w-full p-4 flex justify-center items-center">
+
+            <div className="absolute m-8 w-[calc(100%-64px)] h-[calc(100%-64px)] p-4 top-0 left-0 flex justify-center items-center rounded-lg border border-black bg-[#ccc]">
+                <p className="text-sm text-[#333] text-center">
+                Support MV.GG by whitelisting this site in your Adblock. No intrusive ads I promise!
+                </p>
+            </div>
 
             <ins className="adsbygoogle"
                 style={{display:"inline-block", width:adWidth, height:adHeight}}
