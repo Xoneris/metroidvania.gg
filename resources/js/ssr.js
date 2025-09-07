28c66d2636acea74bfedf5de10664defff2f0e92
Xoneris <kely42@gmx.ch>
2025-09-07 05:54:32 +0200

    Test to maybe make ssr work...?

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -1,16 +1,49 @@
-import { createInertiaApp } from '@inertiajs/react'
-import createServer from '@inertiajs/react/server'
+import path from 'path'
+import fs from 'fs'
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
+import { createInertiaApp } from '@inertiajs/react'
+import createServer from '@inertiajs/react/server'
 
-createServer(page =>
+async function resolvePage(name) {
+  if (typeof import.meta.glob === 'function') {
+    // Vite dev environment
+    const pages = import.meta.glob('./Pages/**/*.tsx', { eager: true })
+    return pages[`./Pages/${name}.tsx`]?.default
+  } else {
+    // Production Node environment
+    const pagesPath = path.resolve('./resources/js/Pages')
+    const file = fs
+      .readdirSync(pagesPath)
+      .find(f => f === `${name}.tsx` || f === `${name}.jsx`)
+
+    if (!file) throw new Error(`Page not found: ${name}`)
+    const module = await import(path.join(pagesPath, file))
+    return module.default
+  }
+}
+
+// async function resolvePage(name) {
+//     // Production Node environment
+//     const pagesPath = path.resolve('./resources/js/Pages')
+//     const file = fs
+//       .readdirSync(pagesPath)
+//       .find(f => f === `${name}.tsx` || f === `${name}.jsx`)
+
+//     if (!file) throw new Error(`Page not found: ${name}`)
+//     const module = await import(path.join(pagesPath, file))
+//     return module.default
+// }
+
+createServer(async page =>
   createInertiaApp({
     page,
     render: ReactDOMServer.renderToString,
-    resolve: name => {
-      const pages = import.meta.glob('./Pages/**/*.tsx', { eager: true })
-      return pages[`./Pages/${name}.tsx`]?.default
-    },
+    // resolve: name => {
+    //   const pages = import.meta.glob('./Pages/**/*.tsx', { eager: true })
+    //   return pages[`./Pages/${name}.tsx`]?.default
+    // },
+    resolve: resolvePage,
     setup: ({ App, props }) => React.createElement(App, props),
   }),
 )
